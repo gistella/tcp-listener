1cd068a7da7780d0626123d9bb4e61725cc964d8
Gianluca Stella <97513570+gistella@users.noreply.github.com>
2023-11-22 12:59:19 +0100

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gistella/tcp-listener
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+)
+
+const defaultPort uint16 = 8080
+
+func main() {
+	var port uint16
+
+	p, err := strconv.ParseUint(os.Getenv("LISTEN_PORT"), 10, 16)
+	if err != nil {
+		port = defaultPort
+	} else {
+		port = uint16(p)
+	}
+
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Listening on port %d\n", port)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(c net.Conn) {
+	fmt.Printf("Accepted connection from %s\n", c.RemoteAddr())
+}
